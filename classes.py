e1912651d026c936c9af25266794abd1bd1df296
hakander <hakanOC78@hotmail.com>
2019-02-13 14:16:52 +0100

    modification des fichier

--- classes.py
+++ classes.py
@@ -1,90 +1,157 @@
+"""Pygame importation."""
 import pygame
 from pygame.locals import *
 
+
 class MacGyver:
-  def __init__(self):
-    self.coord = Coordinates(None, None)
-    self.sprite = pygame.image.load('ressource/MacGyver.png')
+    """
+    Description.
 
-  def get_coord(self):
-    return self.coord
+    A class to define the attributes of Macgyver.
+    """
 
-  def set_coord(self, coord):
-    self.coord = coord
+    def __init__(self):
+        """Constructor. Sets blank coordinates and 'player.png' as sprite."""
+        self.coord = Coordinates(None, None)
+        self.sprite = pygame.image.load('ressource/MacGyver.png').convert_alpha()
 
-  def get_sprite(self):
-    return self.sprite
+    # Getsetters
+    def get_coord(self):
+        """Return Macgyver's position as a Coordinate type object."""
+        return self.coord
 
-  def empty(self):
-    self.sprite = pygame.image.load('ressource/exit.png')
+    def set_coord(self, coord):
+        """Set Macgyver's coordinates. Uses a Coordinate type object as argument."""
+        self.coord = coord
+
+    def get_sprite(self):
+        """Return macgyver's sprite."""
+        return self.sprite
+
+    def empty(self):
+        """Only for aesthetical purposes : makes Macgyver's sprite invisible when he loses."""
+        self.sprite = pygame.image.load('ressource/exit.png').convert_alpha()
+
 
 class Exit:
-  def __init__(self):
-    self.coord = Coordinates(None, None)
-    self.sprite = pygame.image.load('ressource/exit.png')
+    """
+    Description.
 
-  def get_coord(self):
-    return self.coord
+    A class for the Guardian.
+    Same as Macgyver, but wanted to create one because it will be easier for future customization.
+    """
 
-  def set_coord(self, coord):
-    self.coord = coord
+    def __init__(self):
+        """Constructor. Sets blank coordinates and 'exit.png' as sprite."""
+        self.coord = Coordinates(None, None)
+        self.sprite = pygame.image.load('ressource/exit.png').convert_alpha()
 
-  def get_sprite(self):
-    return self.sprite
+    # Getsetters
+    def get_coord(self):
+        """Return the exit's position as a Coordinate type object."""
+        return self.coord
 
-  def empty(self):
-    self.sprite = pygame.image.load('ressource/MacGyver.png') 
+    def set_coord(self, coord):
+        """Set the exit's coordniates. Uses a Coordinate type object as argument."""
+        self.coord = coord
+
+    def get_sprite(self):
+        """Return exit's sprite."""
+        return self.sprite
+
+    def empty(self):
+        """Only for aesthetical purposes : makes the exit's sprite invisible."""
+        self.sprite = pygame.image.load('ressource/MacGyver.png').convert_alpha()
 
 
 class Coordinates:
-  def __init__(self, x, y):
-    self.x = x
-    self.y = y
+    """
+    Description.
 
-  def get_x(self):
-    return self.x
+    Class to create an object to stock the coordinates of the various elements.
+    """
 
-  def get_y(self):
-    return self.y
+    def __init__(self, x, y):
+        """Constructor. Takes two values to set the coordinates : x and y."""
+        self.x = x
+        self.y = y
+
+    # def setCoord(self, x, y):
+        # self.x = x
+        # self.y = y
+
+    def get_x(self):
+        """Return the x value of the coordinates."""
+        return self.x
+
+    def get_y(self):
+        """Return the y value of the coordinates."""
+        return self.y
+
 
 class Square:
-  def __init__(self, coord, is_wall):
-    self.coord = coord
-    self.is_wall = is_wall
-    self.has_item = False
-    self.item = Item(None, 'ressource/empty.png')
+    """
+    Description.
 
-  def get_coord(self):
-    return self.coord
+    A class that will define the squares of the maze.
+    """
 
-  def get_is_wall(self):
-    return self.is_wall
+    def __init__(self, coord, is_wall):
+        """Use Coordinates and isWall boolean as arguments. Starts with no item on it."""
+        self.is_wall = is_wall
+        self.coord = coord
+        self.has_item = False
+        self.item = Item(None, 'ressource/empty.png')
 
-  def get_has_item(self):
-    return self.has_item
+    # Getsetters
+    def get_is_wall(self):
+        """Return a boolean to know if the square is a wall."""
+        return self.is_wall
 
-  def set_has_item(self, state):
-    self.has_item = state
-  
-  def get_item(self):
-    return self.item
+    def get_has_item(self):
+        """Return a boolean to know if there's an item on the square."""
+        return self.has_item
 
-  def set_item(self, item):
-    self.item = item  
+    def set_has_item(self, state):
+        """Set the state of the has_item boolean."""
+        self.has_item = state
+
+    def get_coord(self):
+        """Return the square's position as a Coordinate type object."""
+        return self.coord
+
+    def set_item(self, item):
+        """Change the Item attribute of the square. Uses an Item type object."""
+        self.item = item
+
+    def get_item(self):
+        """Return the item on the square."""
+        return self.item
 
 
 class Item:
-  def __init__(self, name, sprite):
-    self.name = name # pour initialiser et montrer que dans item(none, etc) le none sera remplacer par le nom que j'ai initialiser et qui se trouve dans macgyver.py
-    self.coord = Coordinates(None, None)
-    self.sprite = pygame.image.load(sprite).convert_alpha() #pour dire affiche les sprite c a dire les item tous 
+    """
+    Description.
 
-  def get_coord(self):
-    return self.coord
+    A class to define the items of the maze.
+    """
 
-  def get_sprite(self):
-    return self.sprite 
+    def __init__(self, name, sprite):
+        """Constructor. Name and sprite (file name) as arguments."""
+        self.name = name
+        self.got_item = False
+        self.coord = Coordinates(None, None)
+        self.sprite = pygame.image.load(sprite).convert_alpha()
 
-  def set_coord(self, coord):
-    self.coord = coord          
+    def get_coord(self):
+        """Return the item's position as a Coordinate type object."""
+        return self.coord
+
+    def set_coord(self, coord):
+        """Set the item's coordniates. Use a Coordinate type object as argument."""
+        self.coord = coord
+
+    def get_sprite(self):
+        """Return item's sprite."""
+        return self.sprite          
         
--- methods.py
+++ methods.py
@@ -1,28 +1,31 @@
+"""Pygame, random and class file imoprtation."""
 import random
 import pygame
 from pygame.locals import *
 from classes import *
 
+
 def random_coordinates():
-    """methode qui permet d'obrtenir des coordnée au hazard. retourne un objet de type cordonnées"""
-    return Coordinates(random.randint(0,14), random.randint(0,14))
+    """Method to get random coordinates. Returns a Coordinates type object."""
+    return Coordinates(random.randint(0, 14), random.randint(0, 14))
+
 
 def generate_laby(player, exit):
     """
-    Method to generate the grid.
+    Method to generate the laby.
 
     Transforms the .txt file into a list of lines, then each line into a list of characters.
-    For each character it will generate a square, or set the player/exit's position.
+    For each character it will generate a square, or set Macgyver/exit's position.
     """
-    with open('macgyverlaby.txt') as laby: 
-        str_laby = ''.join(str(line) for line in laby) #expression generatrice (pour les element de ma list '', les element iterable(element dont on peut parcourir les valeurs) c'est join ,  - ici les lignes ), pour parcourir chaque element de la ligne c'est str(line) for ...
-        list_str = list(str_laby) #on dit que chaque ligne c'est une liste 
-        laby = [] # c'est le labyrinthe, la grande liste generale
+    with open('macgyverlaby.txt') as laby:
+        str_laby = ''.join(str(line) for line in laby)
+        list_str = list(str_laby)
+        laby = []
         x = 0
         y = 0
 
         for entry in list_str:
-            # Sets square as a wall if the character is 'W'
+            # Sets square as a wall if the character is 'm'
             if entry == 'm':
                 laby.append(Square(Coordinates(x, y), True))
                 if x < 14:
@@ -31,19 +34,19 @@ def generate_laby(player, exit):
                 elif x == 14:
                     x = 0
                     y = y + 1
-            
 
             elif entry == 'x':
-                laby.append(Square(Coordinates(x,y), False))
+                # Sets square as empty if the character is 'x'
+                laby.append(Square(Coordinates(x, y), False))
                 if x < 14:
-                    x = x + 1 
-                
+                    x = x + 1
+
                 elif x == 14:
                     x = 0
                     y = y + 1
 
-
             elif entry == 'd':
+                # Sets the inital position of the player if the character is a 'd'
                 laby.append(Square(Coordinates(x, y), False))
                 player.set_coord(Coordinates(x, y))
                 if x < 14:
@@ -51,10 +54,10 @@ def generate_laby(player, exit):
 
                 elif x == 14:
                     x = 0
-                    y = y + 1    
+                    y = y + 1
 
             elif entry == 'a':
-                # Sets the position of the exit if the character is a 'E'
+                # Sets the position of the exit if the character is a 'a'
                 laby.append(Square(Coordinates(x, y), False))
                 exit.set_coord(Coordinates(x, y))
                 if x < 14:
@@ -68,7 +71,7 @@ def generate_laby(player, exit):
 
 
 def display_laby(laby, window, wall, background, exit, player):
-    """Method to display the grid."""
+    """Method to display the laby."""
     window.blit(background, (0, 0))
     window.blit(player.get_sprite(), (player.get_coord().get_x() * 30, player.get_coord().get_y() * 30))
     window.blit(exit.get_sprite(), (exit.get_coord().get_x() * 30, exit.get_coord().get_y() * 30))
@@ -80,37 +83,32 @@ def display_laby(laby, window, wall, background, exit, player):
         else:
             if square.get_has_item() is True:
                 window.blit(square.get_item().get_sprite(), (square.get_coord().get_x() * 30, square.get_coord().get_y() * 30))
-    pygame.display.flip()             #si ya des item bah affiche les sprite c a dire un des trois item
+    pygame.display.flip()
 
 
 def put_item_in_laby(laby, item, player, exit):
-    """method pour coller les item dans les position hazardeuse"""
+    """Method to put the items in the laby, at random positions."""
     for square in laby:
-        if square.get_coord().get_x() == item.get_coord().get_x() and square.get_coord().get_y() == item.get_coord().get_y(): 
+        if square.get_coord().get_x() == item.get_coord().get_x() and square.get_coord().get_y() == item.get_coord().get_y():
             if (item.get_coord().get_x() != exit.get_coord().get_x() and item.get_coord().get_y() != exit.get_coord().get_y()) and (item.get_coord().get_x() != player.get_coord().get_x() and item.get_coord().get_y() != player.get_coord().get_y()):
-                if square.get_has_item() is True: #  item(option oui)
-                    item.set_coord(random_coordinates()) #choisir coordonner position hazardeuse
-                    put_item_in_laby(laby, item, player, exit)
-                    break #pour executer deja sa                     
-                if square.get_is_wall() is True: #mur (option oui)
-                    item.set_coord(random_coordinates()) #choisir coordonée position hazardeuse
+                if square.get_has_item() is True:
+                    item.set_coord(random_coordinates())
                     put_item_in_laby(laby, item, player, exit)
                     break
-                if square.get_is_wall() is False and square.get_has_item() is False:#si ya pas de mur ni d'item alors 
-                    square.set_has_item(True)# choisi l'item (ajoute quelque chose, mais quoi ?)
-                    square.set_item(item) # ca(c a dire un des objet)
-                    break #pour sortir de la boucle atout moment    
-                
-            else:                                      #si c la meme position que gardier et macgyver
-                item.set_coord(random_coordinates())  # alors choisi des position hazardeuse
+                if square.get_is_wall() is True:
+                    item.set_coord(random_coordinates())
+                    put_item_in_laby(laby, item, player, exit)
+                    break
+                if square.get_is_wall() is False and square.get_has_item() is False: # si ya pas de mur ni d'item alors
+                    square.set_has_item(True) #ajoute un item
+                    square.set_item(item) # 
+                    break
+            else:
+                item.set_coord(random_coordinates())
                 put_item_in_laby(laby, item, player, exit)
                 break
 
     return laby
-
-# Si j'enleve les break j'ai des aiguiulle partt!
-# si j'enleve les put_itzm_in_laby j'ai pas les items
-
                                 
 
 
